abc635737dce32dbbf6c332071847defaa155545
nyanyacyan <nyanyacyan@gmail.com>
2025-08-25 20:53:44 +0900

    スプシのCode変更

--- installer/src/flow/base/spread_read.py
+++ installer/src/flow/base/spread_read.py
@@ -1,16 +1,13 @@
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$%$$$$$$$$$$$$$$$$$$$
 import gspread
 
- gc = gspread.service_account(
+gc = gspread.service_account(
     filename = "libe-auto-tweet.json"
- )
+)
 
- sh = gc.create(
-        "test_1",
-        folder_id="1ZO3eZuSukBruqwW54IOw7BjiwruUn2po"
- )
+sh = gc.create("test_1", folder_id="1ZO3eZuSukBruqwW54IOw7BjiwruUn2po")
 
- ws = sh.get_worksheet(0)
+ws = sh.get_worksheet(0)
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
@@ -24,8 +21,13 @@ class SpreadsheetReader:
     # ------------------------------------------------------------------------------
     # 関数定義
 
-
-
+    def read_data(self, spreadsheet_id, range_name):
+        gc = gspread.service_account(
+            filename="libe-auto-tweet.json"
+        )
+        sh = gc.open_by_key(spreadsheet_id)
+        ws = sh.get_worksheet(0)
+        return ws.get(range_name)
 
     # ------------------------------------------------------------------------------
     # 関数定義
